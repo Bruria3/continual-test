c92f50f8e3632b64825dacfc1a5f8f1baa5b35f7
bruria <bruria5460@gmail.com>
2022-05-18 00:43:39 +0300

    adding running-time

--- src/continual/test/Program.java
+++ src/continual/test/Program.java
@@ -18,6 +18,7 @@ public class Program {
     }
 
     public static void main(String[] args) {
+        long startTime = System.nanoTime();
         Item t7 = new Item(Operator.AND);
         Item t6 = new Item(Operator.AND);
         Item t5 = new Item(Operator.AND, t6, t7);
@@ -29,6 +30,10 @@ public class Program {
         // this initial data was created only for one scenario - n = 3, in other cases we will need to create more data.
         // so all the others input (6, 8, 32) can be tested with a given array, but without a tree (or by looping from 0 to 2^n and in the body create a new node).
         testResult(3, t1, numbers);
+        long endTime = System.nanoTime();
+        long totalTime = endTime - startTime;
+        System.out.println("total time: " + totalTime);
+
 //        testResult(6);
 //        testResult(8);
 //        testResult(32);
@@ -43,11 +48,9 @@ public class Program {
         //    and no need to pass the first parameter to the getResult function, but instead we can either to random an operator for each node,
         //    or to take it from an array, in that case we will use less memory for creating the tree.
         System.out.println("result: " + test.getResult(tree.getHead(), 0, (int) Math.pow(2, n) - 1, numbers, (int) Math.pow(2, n - 1)));
-        System.out.println("running time: " + Math.pow(2, n));
 
         // complexity
         // for all n inputs the complexity of the algorithm will be O(2^n) because we need to go on each node one time.
-        
         // Thank you:)
     }
 }
